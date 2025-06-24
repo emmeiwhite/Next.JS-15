2d11de2837c594e8890437a3ff9e95e30bb65f0a
emmeiwhite <emmeiwhite@gmail.com>
2025-06-24 17:23:28 +0530

    SSG - Dynamic Params

--- app/blogs/[blogID]/page.jsx
+++ app/blogs/[blogID]/page.jsx
@@ -1,7 +1,24 @@
+/* ---
 export function generateStaticParams() {
   return [{ blogID: '1' }, { blogID: '2' }, { blogID: '3' }, { blogID: '4' }, { blogID: '5' }]
+
+  // let's make an API call to allow next js
+}
+  --- */
+
+export const dynamicParams = false
+
+export async function generateStaticParams() {
+  const response = await fetch('https://dummyjson.com/recipes')
+  const data = await response.json()
+  console.log(data)
+  // Returned array of blogID's on the basis of which Dynamically pages were rendered. Now the same pages are generated at build time, since these are frequently used pages in our application. How awesome is it that NEXT.JS renders the
+  return data.recipes.map(recipe => ({
+    blogID: `${recipe.id}`
+  }))
 }
 
+// One Page to handle Dynamic Routes, which is fantastic
 async function BlogDetail({ params }) {
   const { blogID } = await params
   console.log(blogID)
